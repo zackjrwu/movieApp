bcf92da21a7b93870fe07d1fe0703dddada34286
zack <holyzack1123@hotmail.com>
2023-12-03 00:58:29 +0800

    StarRating Component add PropTypes

--- usepopcorn/src/StarRatimg.js
+++ usepopcorn/src/StarRatimg.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -7,6 +8,16 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.arrayOf(PropTypes.string),
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
