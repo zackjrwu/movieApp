c212f3d28205f6b012709e286a74c0101ed1f153
zack <holyzack1123@hotmail.com>
2024-01-01 19:34:26 +0800

    Add Loader Component

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -54,15 +54,18 @@ const KEY = "f84fc31d";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "interstellar";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
       //  because react state is async, we can't console.log(movies) here
       //  console.log(movies);
       //  but we can console.log(data.Search) here
@@ -78,9 +81,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -89,6 +90,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <div className="loader">Loading...</div>;
 }
 
 function NavBar({ children }) {
